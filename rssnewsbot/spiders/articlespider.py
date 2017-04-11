3a8e064872a2d441b5e2e3d7508485a3e5a91187
hijoe320 <hijoe320@gmail.com>
2017-04-11 02:19:30 +0100

    extract the final url from yahoo rss item

--- rssnewsbot/spiders/articlespider.py
+++ rssnewsbot/spiders/articlespider.py
@@ -9,14 +9,16 @@ import scrapy
 import redis
 import msgpack
 import pymongo as pm
+from colorama import Back, Fore
 from bs4 import BeautifulSoup, Comment, Doctype, CData, ProcessingInstruction, Declaration, Tag
 from ..settings import MONGODB_URI, REDIS_HOST, REDIS_PORT, REDIS_PWD, PENDING_QUEUE
 
 
-def extract_content(page):
+def extract_content(res):
+    page = res.body
     noisy_elms = [Comment, Doctype, CData, ProcessingInstruction, Declaration]
     noisy_tags = ["script", "style", "img", "iframe", "select"]
-    text_tags = ['p', "span", "ul", "td", "li"]
+    text_tags = ['p', "span", "ul", "td"]
 
     def clean_texttag(soup_texttag):
         for child in soup_texttag.contents:
@@ -55,7 +57,7 @@ def extract_content(page):
         # remove footer
         try:
             soup.footer.extract()
-        except:
+        except e:
             pass
         for div in soup.select("#footer"):
             div.extract()
@@ -77,10 +79,24 @@ def extract_content(page):
         wcount = len(text.split())
         container_text[text_tag.parent] += text_tag.get_text(separator=' ', strip=True) + '\n'
         container_wcount[text_tag.parent] += wcount
+    if len(container_wcount) > 0:
+        # Select the container that has biggest word count
+        content_container = max(container_wcount.iteritems(), key=operator.itemgetter(1))[0]
+        return container_text[content_container]
+    else:
+        return None
 
-    # Select the container that has biggest word count
-    content_container = max(container_wcount.iteritems(), key=operator.itemgetter(1))[0]
-    return container_text[content_container]
+
+def extract_final_url(res):
+    page = res.body
+    if page.startswith("<script src="):
+        url = page.split("URL=\'")[-1].split("\'")[0]
+        x = urlparse.urlparse(url)
+        if "=yahoo" in url:
+            url = "{scheme}://{netloc}{path}".format(scheme=x.scheme, netloc=x.netloc, path=x.path)
+        return url
+    else:
+        return None
 
 
 class ArticleSpider(scrapy.Spider):
@@ -97,21 +113,40 @@ class ArticleSpider(scrapy.Spider):
         while True:
             if cmd == "start":
                 self.feed_item = msgpack.unpackb(self.rc.brpop(PENDING_QUEUE))
-                yield scrapy.Request(url=self.feed_item["url"])
+                url = self.feed_item["url"]
+                req = scrapy.Request(url=url)
+                req.headers["User-Agent"] = "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; en-gb) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5"
+                yield scrapy.Request(url=url)
             elif cmd == "stop":
-                logging.debug("article stopped")
+                logging.debug("%sarticle spider stopped", Fore.RED)
                 break
             else:
-                logging.debug("waiting for cmd, set key 'article_spider' to 'start' or 'stop'")
+                logging.debug("%swaiting for cmd, set key 'article_spider' to 'start' or 'stop'", Fore.GREEN)
                 sleep(10)
 
     def parse(self, res):
-        self.feed_item["content"] = extract_content(res.body)
-        self.feed_item["source"] = urlparse.urlparse(res.url).netloc
-        self.feed_item["compressed_html"] = zlib.compress(res.body)
+        logging.debug("%sparsing %s", Fore.LIGHTBLACK_EX, res.url)
         self.feed_item["published_dt"] = datetime.fromtimestamp(self.feed_item["published"])
         self.feed_item["parsed"] = mktime(gmtime())
         self.feed_item["parsed_dt"] = datetime.fromtimestamp(self.feed_item["parsed"])
+        self.parse_page(res)
+
+    def parse_page(self, res):
+        url = extract_final_url(res.body)
+        if url is None:                     # this is the final article page
+            self.feed_item["url"] = res.url
+            self.feed_item["content"] = extract_content(res) or None
+            self.feed_item["compressed_html"] = zlib.compress(res.body)
+            self.update_db()
+        else:                               # true url extracted
+            self.feed_item["url"] = url
+            yield scrapy.Request(url=url)   # parse the final page instead
+
+    def update_db(self):
         _id = self.mc.rssnews.news.insert_one(self.feed_item)
-        self.rc.lpush("nlp", str(_id))
-        logging.debug("parsed %s, mongodb _id=%s", self.feed_item["url"], _id)
+        logging.debug("%sparsed %s, mongodb _id=%s", Back.GREEN, self.feed_item["url"], _id)
+        if self.feed_item["content"] is not None:
+            self.rc.lpush("nlp", str(_id))
+        else:
+            logging.warning("%sfail to extract content, url=%s", Back.RED, self.feed_item["url"])
+        self.feed_item = None
